faf4557e071cd1eae9963fcbe8fde9996f6431b4
hokuto470 <hokuto4910@gmail.com>
2019-10-31 08:11:42 +0900

    header.php custom

--- wp-content/themes/hokutoblog-child/header.php
+++ wp-content/themes/hokutoblog-child/header.php
@@ -19,10 +19,28 @@
 
 	<?php wp_head(); ?>
 </head>
+<!--
+参考サイト 
+https://techacademy.jp
+ABテストツール：https://accounts.google.com/signin/v2/identifier?service=analytics&passive=1209600&continue=https%3A%2F%2Foptimize.google.com%2F&followup=https%3A%2F%2Foptimize.google.com%2F&flowName=GlifWebSignIn&flowEntry=ServiceLogin
+-->
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-<h2><a href="#">記事タイトル</a></h2>
+
+<!-- ▼スニペット Google Tag Manager  ※javaが対応していない場合noscriptタグ配置 -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-XXXXXXXX-Y', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- ▲スニペット End Google Tag Manager  -->
+
+<h2><a href="#"></a></h2>
 
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'hokutoblog' ); ?></a>
 	<header id="masthead" class="site-header">
@@ -45,7 +63,7 @@
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation"><a>
+		<nav id="site-navigation" class="main-navigation navbar-fixed-top bt-navbar-bootcamp header-fixed"><a>
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hokutoblog' ); ?></button>
 			<?php
 			wp_nav_menu( array(
